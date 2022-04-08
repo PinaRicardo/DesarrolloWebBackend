c9d5a0696e1271c2544ca186f686efa3f5048c65
Ricardo Piña <ricardo.pinavargas@gmail.com>
2022-04-07 20:10:09 -0500

    Se creo algo nuevo

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, request
 
 # FlASK
 #############################################################
 app = Flask(__name__)
 #############################################################
 
-@app.route('/prueba')
+@app.route('/pruebacodigo')
 def prueba():
-    return ("A01746551 Prueba")
+   return (" nombres=[]     nombres.append(nombre:ruben,   Semestre01:[{       matematicas: 10,        español10    }]    Semestre02:[{        matematicas: 10,        español:10   }]    })")
 @app.route('/')
 def home():
     return render_template("index.html")
+@app.route('/login', methods=["GET","POST"])
+def login():
+    if (request.methods == "GET"):
+        return render_template("Login.html", error=email)
+    else:
+        email=request.form["email"]
+        password=request.form["password"]
+        return render_template("index.html", error=email)
+
+@app.route('/prueba')
+def prueba():
+    return ("A01746551 Prueba")
