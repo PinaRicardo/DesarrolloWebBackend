4d8970d258f9284e4e3d71c701dc9ac2773001e2
Ricardo Piña <ricardo.pinavargas@gmail.com>
2022-04-07 20:57:51 -0500

    Se creo algo nuevo

--- app.py
+++ app.py
@@ -1,25 +1,52 @@
 from crypt import methods
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+
 
 # FlASK
 #############################################################
 app = Flask(__name__)
+app.permanent_session_lifetime = datetime.timedelta(days=365)
+app.secret_key = "super secret key"
 #############################################################
+
+
+@app.route('/')
+def home():
+    email = None
+    if "email" in session:
+        email = session["email"]
+        return render_template('index.html', data=email)
+    else:
+        return render_template('Login.html', data=email)
 
 @app.route('/pruebacodigo')
 def pruebacodigo():
    return (" nombres=[]     nombres.append(nombre:ruben,   Semestre01:[{       matematicas: 10,        español10    }]    Semestre02:[{        matematicas: 10,        español:10   }]    })")
-@app.route('/')
-def home():
-    return render_template("index.html")
-@app.route('/login', methods=["GET","POST"])
+import email
+from flask import Flask, redirect, render_template, request, session, url_for
+import datetime
+
+@app.route('/signup')
+def signup():
+    name = request.form["name"]
+    email = request.form["email"]
+    password = request.form["password"]
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if (request.method == "GET"):
-        return render_template("login.html", error=email)
+    email = None    
+    if "email" in session:
+        return render_template('index.html', data=session["email"])
     else:
-        email=request.form["email"]
-        password=request.form["password"]
-        return render_template("index.html", error=email)
+        if (request.method == "GET"):
+            return render_template("Login.html", data="email")
+        else:
+            email = request.form["email"]
+            password = request.form["password"]
+            session["email"] = email
+            return render_template("index.html", data=email)
 
 @app.route('/prueba')
 def prueba():
